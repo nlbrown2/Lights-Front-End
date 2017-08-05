f7d4044ffd2e94bb1409c7a2e8620c5b52f5e4ae
Nathan Brown <nlbrown2222@gmail.com>
2017-08-05 01:23:52 -0400

    Use a grid and form to handle radio buttons on left and anything on the right.

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid, Form, Header } from 'semantic-ui-react';
 import BodyImages from 'react-body-images';
 
 const Background = styled.div`
@@ -12,10 +12,6 @@ const Background = styled.div`
   margin-top: -3.6vh;
   align-self: center;
   text-align: center;
-`;
-
-const GridFull = styled(Grid)`
-  height: 100%;
 `;
 
 class HomePage extends Component {
@@ -53,19 +49,39 @@ class HomePage extends Component {
     return (
       <div >
         <BodyImages bgImageArray={['./backgroundRepeat.jpg']} />
-        <GridFull columns="equal">
-          <Grid.Row>
-            <Grid.Column>
-              <p> Text </p>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column style={{backgroundColor: 'red'}} />
-            <Grid.Column style={{backgroundColor: 'green'}}>
-              <p> Text 2 </p>
-            </Grid.Column>
-          </Grid.Row>
-        </GridFull>
+        <div style={{padding: "20%", }}>
+          <div style={{flexDirection: 'row', backgroundColor: 'white', borderBottom: '1px solid'}}>
+            <p style={{paddingLeft: '20%', textAlign: "left"}}>
+              Shows and Options:
+              <span style={{paddingRight: '20%', float: "right"}}>
+                Current Status:
+              </span>
+            </p>
+          </div>
+          <Form style={{flexDirection: 'row', paddingLeft: '5%', backgroundColor: 'white'}}>
+          <Grid divided columns="equal" padded>
+            <Grid.Row>
+              <Grid.Column>
+            <Form.Group style={{flexDirection: "column"}}>
+              <Form.Radio  label="Option 0" value="Option 1" checked={false} onChange={(event, { value }) => console.log(value)}/>
+              <Form.Radio label="Option 1"/>
+              <Form.Radio label="Option 2"/>
+              <Form.Radio label="Option 3"/>
+              <Form.Radio label="Option 4"/>
+              <Form.Radio label="Option 5"/>
+            </Form.Group>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Idle?</p>
+            <p>Idle?</p>
+            <p>Idle?</p>
+            <p>Idle?</p>
+            <p>Idle?</p>
+          </Grid.Column>
+        </Grid.Row>
+        </Grid>
+          </Form>
+        </div>
       </div>
     );
   }
