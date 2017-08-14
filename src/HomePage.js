6046643be2a83801dec2a7607a635b83c135af5d
Nathan Brown <nlbrown2222@gmail.com>
2017-08-14 00:53:57 -0400

    Send request with user options and type checking.

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { List, Input, Icon, Table, Button, Header } from 'semantic-ui-react';
 import BodyImages from 'react-body-images';
-import ShowMore from 'react-show-more';
+import ShowMoreLess from './ShowMoreLess.js';
 
 class HomePage extends Component {
 
@@ -22,7 +22,210 @@ class HomePage extends Component {
     this.setState({ selectedRow: index })
   }
 
-  //need: show name, show description, and position in queue.
+  sendRequest(index){
+    if(this.validOptions()) {
+      let show_name = this.props.shows[this.state.selectedRow].name;
+      this.props.sendRequest(show_name, this.state.options[show_name]);
+    }
+  }
+
+  validOptions(){
+    // return true; used for testing backend with invalid options
+    if(this.state.selectedRow === -1){
+      return false;
+    }
+    console.log(this.props.shows[this.state.selectedRow].options);
+    var showObj = this.props.shows[this.state.selectedRow];
+    let show_name = showObj.name;
+    if(this.state.options[show_name]){
+      console.log(Object.keys(this.state.options[show_name]));
+      for(let i = 0; i < Object.keys(this.state.options[show_name]).length; i++) {
+        let optionObj = showObj.options[Object.keys(this.state.options[show_name])[i]];
+        console.log(optionObj, 'option obj');
+        if(optionObj){
+          let userInput = this.state.options[show_name][Object.keys(this.state.options[show_name])[i]];
+          let type = showObj.options[Object.keys(this.state.options[show_name])[i]].type;
+          console.log(userInput);
+          if(type === "Float") {
+            userInput = parseFloat(userInput);
+            if(isNaN(userInput)) {
+              console.log('invalid Floating point number');
+              return false;
+            } else if(!this.withinBounds(userInput, optionObj)){
+              console.log('invalid Floating point number: too low or too high');
+              return false;
+            }
+          } else if(type === "Integer"){
+            userInput = parseInt(userInput);
+            if(isNaN(userInput)){
+              console.log('invalid Integer');
+              return false;
+            } else if(!this.withinBounds(userInput, optionObj)){
+              console.log('invalid Floating point number: too low or too high');
+              return false;
+            }
+          } else if(type === "Boolean"){
+            if(userInput.toLowerCase() !== 'true' || userInput.toLowerCase() !== 'false'){
+              console.log('invalid boolean', userInput.toLowerCase());
+              return false;
+            } else if(!this.withinBounds(userInput, optionObj)){
+              console.log('invalid Floating point number: too low or too high');
+              return false;
+            }
+          } else if(type === "List of Floats"){
+            try {
+              userInput = JSON.parse(userInput);
+            } catch (e) {
+              console.log('Invalid list of floats', e);
+              return false;
+            }
+          } else {
+            console.log('not checking this type', type);
+          }
+          console.log(type);
+          console.log(userInput);
+          console.log('passed all checks');
+        } else {
+          console.log(Object.keys(this.state.options[show_name])[i]);
+        }
+      }
+      return true;
+    } else {
+      console.log('no user options');
+      return true;
+    }
+    console.log('end of function');
+    return false;
+  }
+
+  withinBounds(user_value, option_obj){
+    console.log(user_value, option_obj);
+    if(option_obj['type'] === 'Float'){
+      if(typeof(option_obj['lowerBound']) === typeof('')){
+        //this means the lower bound is another option
+        let other_option = option_obj['lowerBound'];
+        if(this.state.options[this.props.shows[this.state.selectedRow].name][other_option]){
+          //the user has inputted the other option, so parse the string and check 
+          try{
+            let other_value = parseFloat(this.state.options[this.props.shows[this.state.selectedRow].name][other_option]);
+            if(user_value < other_value){
+              return false;
+            }
+          } catch (e){
+            alert('please ensure', other_option, 'is valid');
+            console.log('error with ', other_option, e);
+          }
+        } else {
+          //use default value
+          let other_value = option_obj['default'];
+          if(user_value < other_value){
+            return false;
+          }
+        }
+      } else {
+        //use lower bound from option_obj
+        let lower_bound = option_obj['lowerBound'];
+        if(user_value < lower_bound){
+          return false;
+        }
+      }
+      if(typeof(option_obj['upperBound']) === typeof('')){
+        //this means the upper bound is another option
+        let other_option = option_obj['upperBound'];
+        if(this.state.options[this.props.shows[this.state.selectedRow].name][other_option]){
+          //the user has inputted the other option, so parse the string and check 
+          try{
+            let other_value = parseFloat(this.state.options[this.props.shows[this.state.selectedRow].name][other_option]);
+            if(user_value > other_value){
+              return false;
+            }
+          } catch (e){
+            alert('please ensure', other_option, 'is valid');
+            console.log('error with ', other_option, e);
+          }
+        } else {
+          //use default value
+          let other_value = option_obj['default'];
+          if(user_value > other_value){
+            return false;
+          }
+        }
+      } else {
+        //use upper bound from option object
+        let upper_bound = option_obj['upperBound'];
+        if(user_value > upper_bound){
+          return false;
+        }
+      }
+      return true;
+    }
+    if(option_obj['type'] === 'Integer'){
+      if(typeof(option_obj['lowerBound']) === typeof('')){
+        //this means the lower bound is another option
+        let other_option = option_obj['lowerBound'];
+        if(this.state.options[this.props.shows[this.state.selectedRow].name][other_option]){
+          //the user has inputted the other option, so parse the string and check 
+          try{
+            let other_value = parseInt(this.state.options[this.props.shows[this.state.selectedRow].name][other_option]);
+            if(user_value < other_value){
+              return false;
+            }
+          } catch (e){
+            alert('please ensure', other_option, 'is valid');
+            console.log('error with ', other_option, e);
+          }
+        } else {
+          //use default value
+          let other_value = option_obj['default'];
+          if(user_value < other_value){
+            return false;
+          }
+        }
+      } else {
+        //use lower bound from option_obj
+        let lower_bound = option_obj['lowerBound'];
+        if(user_value < lower_bound){
+          return false;
+        }
+      }
+      if(typeof(option_obj['upperBound']) === typeof('')){
+        //this means the upper bound is another option
+        let other_option = option_obj['upperBound'];
+        if(this.state.options[this.props.shows[this.state.selectedRow].name][other_option]){
+          //the user has inputted the other option, so parse the string and check
+          try{
+            let other_value = parseInt(this.state.options[this.props.shows[this.state.selectedRow].name][other_option]);
+            if(user_value > other_value){
+              return false;
+            }
+            console.log(other_value,'upper bound user inputted');
+            console.log(user_value, 'user_value');
+          } catch (e){
+            alert('please ensure', other_option, 'is valid');
+            console.log('error with ', other_option, e);
+          }
+        } else {
+          //use default value
+          let other_value = option_obj['default'];
+          if(user_value > other_value){
+            return false;
+          }
+            console.log(other_value,'upper bound default');
+            console.log(user_value, 'user_value');
+        }
+      } else {
+        //use upper bound from option object
+        let upper_bound = option_obj['upperBound'];
+        if(user_value > upper_bound){
+          return false;
+        }
+            console.log(upper_bound,'upper bound default');
+            console.log(user_value, 'user_value');
+      }
+      return true;
+    }
+  }
+
   updateOptionValue(show_index, option_name, value){
     let old_options = this.state.options;
     let show_name = this.props.shows[show_index].name;
